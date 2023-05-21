ce617e00f1ea8be63d18bf9ea65ef44498b2d27b
wells <jhang0912407249@gmail.com>
2023-05-21 18:55:59 +0800

    style: change portfolio card css

--- components/portfolio/card.tsx
+++ components/portfolio/card.tsx
@@ -43,19 +43,19 @@ export default function Card({ project }: { project: Project }) {
         <button onClick={closeModal} className="w-full h-10 text-center text-white bg-red-700">close</button>
       </Modal>
 
-      <div ref={element} className={`hover:scale-110 mx-auto max-w-xs bg-white font-notosans transition-all duration-500 hover:cursor-pointer ${reveal ? 'opacity-100' : 'translate-y-20 opacity-0'}`} onClick={openModal}>
+      <div ref={element} className={`mx-auto max-w-xs bg-white font-notosans transition-all duration-500 hover:cursor-pointer ${reveal ? 'opacity-100' : 'translate-y-20 opacity-0'} flex flex-col`} onClick={openModal}>
         <img className="object-scale-down object-center bg-gray-300 h-60 w-80" src={project.image} alt={project.title} />
-        <div className="mx-6 mt-5 text-lg">
+        <div className="flex flex-col justify-around flex-1 px-5 py-6 text-lg">
           <h2 className="font-bold text-secondary">{project.title}</h2>
           <p className="mt-5 text-sm line-clamp-5 text-zinc-700">{project.description}</p>
-        </div>
-        <div className="pb-8 mx-6 mt-8">
-          {
-            project.demo_link && <a className="px-4 py-2 text-sm mr-2 text-white border rounded-[10px] border-secondary hover:no-underline bg-secondary hover:bg-white hover:text-secondary" href={project.demo_link}>Demo</a>
-          }
-          {
-            project.code_link && <a className="px-4 py-2 text-sm text-white border rounded-[10px] border-secondary hover:no-underline bg-secondary hover:bg-white hover:text-secondary" href={project.code_link}>Source Code</a>
-          }
+          <div className="mt-8">
+            {
+              project.demo_link && <a className="px-4 py-2 text-sm mr-2 text-white border rounded-[10px] border-secondary hover:no-underline bg-secondary hover:bg-white hover:text-secondary" href={project.demo_link}>Demo</a>
+            }
+  {
+    project.code_link && <a className="px-4 py-2 text-sm text-white border rounded-[10px] border-secondary hover:no-underline bg-secondary hover:bg-white hover:text-secondary" href={project.code_link}>Source Code</a>
+  }
+          </div>
         </div>
       </div>
     </>
