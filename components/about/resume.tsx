238007296399dca55d67e4e98b5ad75488ec9571
wells <jhang0912407249@gmail.com>
2023-05-24 10:54:01 +0800

    feat: change resume link

--- components/about/resume.tsx
+++ components/about/resume.tsx
@@ -2,9 +2,9 @@ export default function Resume() {
   return (
     <div className="pb-32 mt-32 text-center print:hidden">
       <h2 className="text-center text-white font-notosans shadow-gray-700 text-7xl">Download</h2>
-      <button onClick={()=>{window.print()}} className="mt-20 inline-block border-2 border-cyan-450 text-white px-5 pt-3 pb-2 rounded-[10px] bg-cyan-450 dark:hover:border-primary hover:bg-white hover:text-cyan-450 hover:no-underline">
+      <a className="mt-20 inline-block border-2 border-cyan-450 text-white px-5 pt-3 pb-2 rounded-[10px] bg-cyan-450 dark:hover:border-primary hover:bg-white hover:text-cyan-450 hover:no-underline" href="https://www.canva.com/design/DAFiDaI9y04/T1hobxDDqXhRGJSVzvXraw/view?utm_content=DAFiDaI9y04&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank" >
         下載我的履歷
-      </button>
+      </a>
     </div>
   )
 }
