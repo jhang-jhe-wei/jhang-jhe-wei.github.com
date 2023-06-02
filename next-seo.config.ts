7c9083707d038822b9aa5b05f3e604ebeac1ba90
wells <jhang0912407249@gmail.com>
2023-06-02 20:06:53 +0800

    chore: add i18n to seo index page

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,18 +1,19 @@
 import { DefaultSeoProps } from 'next-seo'
 
 const config: DefaultSeoProps = {
-  title: "Wells's Website",
-  description: "In here, you can find my projects, blogs, and some other stuffs.",
+  title: "Wells 的個人網站",
+  description: "在這裡，你可以找到我的專案、部落格、以及其他一些東西。",
   canonical: "https://wells.tw/",
   openGraph: {
-    url: 'https://i.imgur.com/qtzOumT.png',
-    title: "Wells's Website",
-    description: "In here, you can find my projects, blogs, and some other stuffs.",
+    url: 'https://wells.tw/',
+    title: "Wells 的個人網站",
+    description: "在這裡，你可以找到我的專案、部落格、以及其他一些東西。",
     images: [
       { url: 'https://i.imgur.com/qtzOumT.png' }
     ],
     type: 'website',
-    siteName: "Wells's Website"
+    siteName: "Wells's Website",
+    locale: 'zh_TW'
   }
 }
 
