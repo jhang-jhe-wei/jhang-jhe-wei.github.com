48dad46151e880d39698d8e5fda8b7ef61b31a90
wells <jhang0912407249@gmail.com>
2023-06-02 22:09:59 +0800

    chore: add ga4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,15 +7,47 @@ import { Provider } from 'react-redux'
 import { useEffect } from 'react'
 import Modal from 'react-modal'
 import { appWithTranslation } from 'next-i18next'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 Modal.setAppElement('#__next')
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
-    <Provider store={store}>
-      <ModeEffect />
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
+      <Provider store={store}>
+        <ModeEffect />
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </Provider>
+    </>
   )
 }
 
