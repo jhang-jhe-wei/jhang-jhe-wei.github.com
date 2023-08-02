7e18305940c485096344ebef00eeba28e7eac409
wells <jhang0912407249@gmail.com>
2023-08-02 12:37:43 +0800

    feat: replace posts with issues

--- pages/posts/page/[page].tsx
+++ pages/posts/page/[page].tsx
@@ -1,37 +1,43 @@
-import { NextSeo } from 'next-seo'
-import tinytime from 'tinytime'
-import Pagination from '../../../components/blog/pagination'
-import { GetStaticProps } from 'next'
-import Layout from '../../../components/layout'
-import * as posts from '../../../lib/posts'
-import type { Post } from '../../../lib/posts'
-import SectionContainer from '../../../components/blog/SectionContainer'
-import Link from 'next/link'
-import { useTranslation } from 'next-i18next'
+import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { useAppDispatch } from 'reducers/store'
-import { changeLanguage } from 'reducers/locale_slice'
-import { useEffect } from 'react'
 import { i18n } from 'next-i18next.config'
+import { useAppDispatch } from '@/reducers/store'
+import { changeLanguage } from '@/reducers/locale_slice'
+import { useEffect } from 'react'
+import { NextSeo } from 'next-seo'
+import Pagination from '../../../components/blog/pagination'
+import Layout from '../../../components/layout'
+import Link from 'next/link'
 import DefaultSeo from '../../../next-seo.config'
+import { useTranslation } from 'next-i18next'
+import GithubAPI from '@/lib/githubAPI'
+import tinytime from 'tinytime'
 
-interface PostsPageProps {
-  totalPage: number
-  page: number
+interface Post {
+  title: string
+  createdAt: string
+  updatedAt: string
+  id: number
+  labels: string[]
+}
+
+interface PostProps {
   posts: Post[]
   locale: typeof i18n.locales[number]
+  page: number
+  totalPage: number
 }
 
 const postDateTemplate = tinytime('{MMMM} {DD}, {YYYY}')
-export default function PostsPage (props: PostsPageProps): React.ReactElement {
+export default function PostPage (props: PostProps): React.ReactElement {
   const {
-    totalPage,
-    page,
     posts,
-    locale
+    locale,
+    page,
+    totalPage
   } = props
-  const { t } = useTranslation()
   const dispatch = useAppDispatch()
+  const { t } = useTranslation()
   useEffect(() => {
     dispatch(changeLanguage(locale))
   }, [])
@@ -44,14 +50,14 @@ export default function PostsPage (props: PostsPageProps): React.ReactElement {
         canonical={'https://wells.tw/posts/page'}
         openGraph={{
           ...DefaultSeo.openGraph,
-          locale,
-          url: 'https://wells.tw/posts/page',
-          title: t('blog'),
-          description: t('blogDescription')
+            locale,
+            url: 'https://wells.tw/posts/page',
+            title: t('blog'),
+            description: t('blogDescription')
         }}
       />
       <Layout>
-        <SectionContainer>
+        <div className="container mx-auto">
           <main>
             <div className="divide-y divide-gray-200">
               <div className="pt-6 pb-8 space-y-2 md:space-y-5">
@@ -65,33 +71,33 @@ export default function PostsPage (props: PostsPageProps): React.ReactElement {
               <ul className="divide-y divide-gray-200">
                 {posts.map(post => {
                   return (
-                    <li key={post.slug} className="py-4">
+                    <li key={post.title} className="py-4">
                       <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
                         <dl>
                           <dt className="sr-only">Published on</dt>
                           <dd className="text-base font-medium text-primary dark:text-light">
-                            <time dateTime={post.options.createdAt}>
-                              {postDateTemplate.render(new Date(post.options.createdAt))}
+                            <time dateTime={post.createdAt}>
+                              {postDateTemplate.render(new Date(post.createdAt))}
                             </time>
                           </dd>
                         </dl>
-                        <div className="space-y-5 xl:col-span-3">
-                          <div className="space-y-6">
+                        <div className="space-y-3 xl:col-span-3">
+                          <div className="space-y-2">
                             <h2 className="text-2xl font-bold tracking-tight">
-                              <Link href={`/posts/${post.slug}`} className="text-primary dark:text-light">
-                                {post.slug}
+                              <Link href={`/posts/${post.id}`} className="text-primary dark:text-light">
+                                {post.title}
                               </Link>
                             </h2>
                             <div className="text-primary dark:text-light prose max-w-none">
-                              {post.options.description}
+                              { post.labels.map((label) => `#${label}`).join(' ') }
                             </div>
                           </div>
                           <div className="text-base font-medium">
                             <Link
-                              href={`/posts/${post.slug}`}
+                              href={`/posts/${post.id}`}
                               className="text-teal-600 hover:text-teal-700"
-                              aria-label={`Read "${post.slug}"`}>
-                              Read more &rarr;
+                              aria-label={`Read "${post.title}"`}>
+                              Go to read &rarr;
                             </Link>
                           </div>
                         </div>
@@ -105,35 +111,45 @@ export default function PostsPage (props: PostsPageProps): React.ReactElement {
           <div className="flex justify-center pt-8 pb-4">
             <Pagination currentPage={page} totalPage={totalPage}/>
           </div>
-        </SectionContainer>
+        </div>
       </Layout>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps<PostsPageProps> = async (context) => {
-  const totalPage = await posts.totalPages()
-  const page = parseInt(context.params.page as string)
+export const getServerSideProps: GetServerSideProps<PostProps> = async (context) => {
   const {
-    locale
+    locale,
+    params: { page }
   } = context
+
+  const result = await GithubAPI.request('GET /repos/{owner}/{repo}/issues', {
+    owner: 'jhang-jhe-wei',
+    repo: 'jhang-jhe-wei.github.com',
+    state: 'closed',
+    sort: 'updated',
+    page: Number(page),
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+
+  const issues = result.data.filter((issue) => !issue.hasOwnProperty('pull_request')).map((issue) => ({
+    title: issue.title,
+    id: issue.number,
+    createdAt: issue.created_at,
+    updatedAt: issue.updated_at,
+    labels: issue.labels.map((label) => label.name)
+  }))
+
   return {
     props: {
-      totalPage,
-      page,
-      posts: await posts.page(page),
+      posts: issues,
       locale,
       ...(await serverSideTranslations(locale, [
         'common'
       ]))
-    }
-  }
-}
 
-export const getStaticPaths = async ({ locales }) => {
-  const paths = await posts.genIndexPaths(locales)
-  return {
-    paths,
-    fallback: false
+    }
   }
 }
